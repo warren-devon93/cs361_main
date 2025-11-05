706e138ce03f0c2961d7e11fd7ccc6166dbaf2fc
Devon Warren <warren.devon93@pm.me>
2025-11-05 09:59:32 -0800

    Progress made on button based recipe page UI

--- mainwindow.py
+++ mainwindow.py
@@ -1,5 +1,5 @@
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QApplication, QMainWindow, QMessageBox, QTableWidget
+from PySide6.QtWidgets import QMainWindow
 from ui_mainwindow import Ui_mainWindow
     
 class MainWindow(QMainWindow, Ui_mainWindow):
@@ -17,22 +17,63 @@ class MainWindow(QMainWindow, Ui_mainWindow):
         self.stackedWidget.setParent(self.tabWidget)
         self.stackedWidget.move(21, 325)
         # TODO: Set up text wraps and max string length limits for all table fields
+        # Set stacked widget parent and location
+        self.stackedWidget.setParent(self.tabWidget)
+        self.stackedWidget.move(21, 325)
         # Set button signals
         self.addButton.clicked.connect(self.addButtonClicked)
         self.backpageButton.clicked.connect(self.backpageButtonClicked)
+        self.addRowButton.clicked.connect(self.addRowButtonClicked)
+        self.removeRowButton.clicked.connect(self.removeRowButtonClicked)
         self.ingredientsTable.itemChanged.connect(self.recipeChanged)
         self.instructionsTable.itemChanged.connect(self.recipeChanged)
         self.addItemButton.clicked.connect(self.addItemButtonClicked)
         self.removeItemButton.clicked.connect(self.removeItemButtonClicked)
         self.app = app
 
+    def tablesPopulated(self):
+        tables = [self.ingredientsTable, self.instructionsTable]
+        for table in tables:
+            for row in range(table.rowCount()):
+                for column in range(table.columnCount()):
+                    item = table.item(row, column)
+                    if item is None or not item.text():
+                        return False
+        return True
+
+    def getTable(self):
+        if self.tabWidget.currentWidget() is self.ingredientsTab:
+            return self.ingredientsTable
+        else:
+            return self.instructionsTable        
+
     # Slots
+    def addRowButtonClicked(self):
+        # Retrieve current table
+        table = self.getTable()
+        # Append row to end of table
+        table.insertRow(table.rowCount())
+        # Disable add row button
+        self.addRowButton.setEnabled(False)
+        self.removeRowButton.setEnabled(True)
+    
+    def removeRowButtonClicked(self):
+        # Retrieve current table
+        table = self.getTable()
+        if table.currentRow()>=0:
+            # Remove selected row from table
+            table.removeRow(table.currentRow())
+        else:
+            # Remove last row from table if no row selected
+            table.removeRow(table.rowCount()-1)
+
     def addButtonClicked(self):
-        # change disabled status of toolbar buttons
-        self.backpageButton.setDisabled(False)
-        self.addButton.setDisabled(True)
-        self.saveButton.setDisabled(True)
-        self.uploadButton.setDisabled(True)
+        # change enabled status of buttons
+        self.backpageButton.setEnabled(True)
+        self.addButton.setEnabled(False)
+        self.uploadButton.setEnabled(False)
+        self.addRowButton.setEnabled(False)
+        self.removeRowButton.setEnabled(False)
         # Insert initial blank rows in ingredients and instructions tables
         self.ingredientsTable.insertRow(0)
         self.instructionsTable.insertRow(0)
@@ -42,21 +83,21 @@ class MainWindow(QMainWindow, Ui_mainWindow):
 
     def backpageButtonClicked(self):
         # Set disabled status of toolbar buttons
-        self.backpageButton.setDisabled(True)
-        self.addButton.setDisabled(False)
-        self.editButton.setDisabled(True)
-        self.makeListButton.setDisabled(True)
-        self.deleteButton.setDisabled(True)
-        self.saveButton.setDisabled(True)
-        self.uploadButton.setDisabled(False)
+        self.backpageButton.setEnabled(False)
+        self.addButton.setEnabled(True)
+        self.editButton.setEnabled(False)
+        self.makeListButton.setEnabled(False)
+        self.deleteButton.setEnabled(False)
+        self.saveButton.setEnabled(False)
+        self.uploadButton.setEnabled(True)
         # Set stacked widget index to recipePage
         self.stackedPages.setCurrentWidget(self.catalogPage)
     
     def editButtonClicked(self):
         # Change disabled status of toolbar buttons
-        self.backpageButton.setDisabled(False)
-        self.editButton.setDisabled(True)
-        self.uploadButton.setDisabled(True)
+        self.backpageButton.setEnabled(True)
+        self.editButton.setEnabled(False)
+        self.uploadButton.setEnabled(False)
         # TODO: Populate rows of ingredients and instructions tables with recipe data
         # Set stacked widget index to recipe page
         self.stackedPages.setCurrentWidget(self.recipePage)
@@ -64,27 +105,47 @@ class MainWindow(QMainWindow, Ui_mainWindow):
     
     def makeListButtonClicked(self):
         # Change disabled status of toolbar buttons
-        self.backpageButton.setDisabled(False)
-        self.makeListButton.setDisabled(True)
-        self.uploadButton.setDisabled(True)
+        self.backpageButton.setEnabled(True)
+        self.makeListButton.setEnabled(False)
+        self.uploadButton.setEnabled(False)
         # TODO: Populate rows of grocery list table with ingredients data
         # TODO: Set stacked widget to grocery list page
 
-    def addItemButtonClicked(self):
-        if self.tabWidget.currentWidget() is self.ingredientsTab:
-            table = self.ingredientsTable
-        else:
-            table = self.instructionsTable
-        table.insertRow(table.rowCount())
+    # Enables addRowButton, removeRowButton, and saveButton based on status of each table
+    def recipeChanged(self):
+        table = self.getTable()
+        row = table.currentRow()
+        blanks = 0
+        # Searches current row for blank cells
+        for column in range(table.columnCount()):
+            item = table.item(row, column)
+            if item is None or not item.text():
+                blanks+=1
+        match blanks:
+            case 0:
+                # Enable addRowButton if table populated
+                # TODO: Control structure
+                self.addRowButton.setEnabled(True)
+                # Enable save button if both tables entirely populated
+                if self.saveButton.isEnabled() is False and self.tablesPopulated():
+                    self.saveButton.setEnabled(True)
+                return
+            case 1:
+                # Disables save button if current row only partially populated
+                self.saveButton.setEnabled(False)
+                self.addRowButton.setEnabled(False)
+                return
+            case _:
+                # Removes blank row from table
+                table.removeRow(table.currentRow())
+                # Enable save button if both tables entirely populated
+                if self.saveButton.isEnabled() is False and self.tablesPopulated():
+                    self.saveButton.setEnabled(True)
+                return
+            
 
-    def removeItemButtonClicked(self):
-        if self.tabWidget.currentWidget() is self.ingredientsTab:
-            table = self.ingredientsTable
-        else:
-            table = self.instructionsTable
-        table.removeRow(table.currentRow())
-        # TODO: Figure out how to call recipeChanged to check if removal makes both tables full
 
+"""
     def recipeChanged(self):
         tables = [self.ingredientsTable, self.instructionsTable]
         for table in tables:
